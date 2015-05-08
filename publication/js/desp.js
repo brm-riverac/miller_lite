5dc6312b0d723bfb02b0a88d7175dd0b45738ddb
brm-pintoc <pintoc@iMac-de-Brm-3.local>
2015-05-07 23:33:01 -0500

    galeria con estado

--- publication/js/desp.js
+++ publication/js/desp.js
@@ -1,13 +1,39 @@
-
-      $(document).ready(function(){
+$(document).ready(function(){
+		  
        $(".tog1").click(function(){
            $(".desp1").toggle( "slow", "linear" );
        });
-      });
+     
 
 
-      $(document).ready(function(){
+      
        $(".tog2").click(function(){
            $(".desp2").toggle( "slow", "linear" );
        });
-      });
+      
+
+	   $(".statusXPUser").click(function(){
+		   var location = $(this).attr("data-expand");
+		   
+		   if(location=='showStatus'){
+			   //jQuery('.statusXPUser[data-expand='+location+']').addClass('showStatusbar');
+			   jQuery('.statusXPUser[data-expand='+location+']').addClass('showStatusbar');
+			   jQuery('.insideStatus[data-expand='+location+']').addClass('insideStatusOpen');
+			   //jQuery('.insideStatus[data-expand='+location+']').show('fade');
+			   console.log('cargo funcion 1');
+			 }else if(location=='showStatus2'){
+				 jQuery('.statusXPUser[data-expand='+location+']').addClass('showStatusbar');
+				 jQuery('.insideStatus[data-expand='+location+']').addClass('insideStatusOpen');
+				   console.log('cargo funcion 2');
+			 }else if(location=='showStatus3'){
+				 jQuery('.statusXPUser[data-expand='+location+']').addClass('showStatusbar');
+				 jQuery('.insideStatus[data-expand='+location+']').addClass('insideStatusOpen');
+				 console.log('cargo funcion 3');
+				 
+			 }
+			 
+			 
+		   
+		   });
+	   
+});
