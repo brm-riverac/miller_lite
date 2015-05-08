fc6850e7e7a7093fbb746a3c2d23fae4961cac99
Camilo <camilo.rivera@brm.com.co>
2015-05-07 19:23:56 -0500

    hability

---
+++ publication/js/desp.js
@@ -0,0 +1,13 @@
+
+      $(document).ready(function(){
+       $(".tog1").click(function(){
+           $(".desp1").toggle( "slow", "linear" );
+       });
+      });
+
+
+      $(document).ready(function(){
+       $(".tog2").click(function(){
+           $(".desp2").toggle( "slow", "linear" );
+       });
+      });
