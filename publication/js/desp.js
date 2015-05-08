21d30fc7feba83eff20a081fd24bcb3dd94096ce
Camilo <camilo.rivera@brm.com.co>
2015-05-07 23:49:45 -0500

    hy

--- publication/js/desp.js
+++ publication/js/desp.js
@@ -3,10 +3,7 @@
        $(".tog1").click(function(){
            $(".desp1").toggle( "slow", "linear" );
        });
-      });
-
-
-      $(document).ready(function(){
+      
        $(".tog2").click(function(){
            $(".desp2").toggle( "slow", "linear" );
        });
